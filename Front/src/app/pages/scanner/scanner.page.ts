8952597a71959bbe5a7f74ce2e6cd9ddb753af09
Milan-VERY-GRIETTE <46976927+TymeOn@users.noreply.github.com>
2022-11-14 14:18:46 +0100

    feat: front scanner and android

--- Front/src/app/pages/scanner/scanner.page.ts
+++ Front/src/app/pages/scanner/scanner.page.ts
@@ -1,14 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.page.html',
   styleUrls: ['./scanner.page.scss'],
 })
-export class ScannerPage implements OnInit {
+export class ScannerPage implements OnInit, AfterViewInit {
+
+  cameraAllowed = false;
+  isScanning = false;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.checkPermission().then();
+  }
+
+  ngAfterViewInit() {
+    BarcodeScanner.prepare().then();
+  }
+
+  async checkPermission() {
+    const status = await BarcodeScanner.checkPermission({ force: true });
+    this.cameraAllowed = status.granted;
+  }
+
+  async startScan() {
+    this.isScanning = true;
+
+    document.querySelector('body').classList.add('scanner-active');
+    BarcodeScanner.hideBackground();
+
+    const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+    if (result.hasContent) {
+      console.log(result.content); // log the raw scanned content
+    }
+
+    document.querySelector('body').classList.remove('scanner-active');
+
+    this.isScanning = false;
+  }
+
+  stopScan() {
+    BarcodeScanner.stopScan().then();
+    document.querySelector('body').classList.remove('scanner-active');
+    this.isScanning = false;
+  }
 
 }
